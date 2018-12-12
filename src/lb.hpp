7eae139e216f5acfc08585b5d54736ccb840f620
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-12-12 09:05:43 +0100

    Improved template version for inner product.

--- src/lb.hpp
+++ src/lb.hpp
@@ -41,25 +41,25 @@ std::array<T, 19> lb_calc_m_from_n(const std::array<T, 19> &n) {
 template <typename T>
 std::array<T, 19> lb_calc_m_from_n_template(const std::array<T, 19> &n) {
   std::array<T, 19> m;
-  m[0] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_0>(n);
-  m[1] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_1>(n);
-  m[2] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_2>(n);
-  m[3] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_3>(n);
-  m[4] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_4>(n);
-  m[5] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_5>(n);
-  m[6] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_6>(n);
-  m[7] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_7>(n);
-  m[8] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_8>(n);
-  m[9] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_9>(n);
-  m[10] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_10>(n);
-  m[11] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_11>(n);
-  m[12] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_12>(n);
-  m[13] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_13>(n);
-  m[14] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_14>(n);
-  m[15] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_15>(n);
-  m[16] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_16>(n);
-  m[17] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_17>(n);
-  m[18] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki_18>(n);
+  m[0] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 0>(n);
+  m[1] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 1>(n);
+  m[2] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 2>(n);
+  m[3] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 3>(n);
+  m[4] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 4>(n);
+  m[5] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 5>(n);
+  m[6] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 6>(n);
+  m[7] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 7>(n);
+  m[8] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 8>(n);
+  m[9] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 9>(n);
+  m[10] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 10>(n);
+  m[11] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 11>(n);
+  m[12] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 12>(n);
+  m[13] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 13>(n);
+  m[14] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 14>(n);
+  m[15] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 15>(n);
+  m[16] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 16>(n);
+  m[17] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 17>(n);
+  m[18] = Utils::inner_product_template<T, 19, ::D3Q19::e_ki, 18>(n);
   return m;
 }
 
--- src/utils/inner_product_template.hpp
+++ src/utils/inner_product_template.hpp
@@ -6,37 +6,39 @@
 
 namespace Utils {
 
-using Array = std::array<double, 19>;
-
 template <int c> constexpr double mul(double ai) {
   return (c == 1) ? ai : c * ai;
 }
 
-template <int I, int c, int... cs> struct inner_product_template_impl {
-  constexpr double operator()(Array const &a) const {
+template <int I, typename T, std::size_t N, int c, int... cs>
+struct inner_product_template_impl {
+  constexpr T operator()(std::array<T, N> const &a) const {
     return (c) ? mul<c>(std::get<I>(a)) +
-                     inner_product_template_impl<I + 1, cs...>{}(a)
-               : inner_product_template_impl<I + 1, cs...>{}(a);
+                     inner_product_template_impl<I + 1, T, N, cs...>{}(a)
+               : inner_product_template_impl<I + 1, T, N, cs...>{}(a);
   }
 };
 
-template <int I, int c> struct inner_product_template_impl<I, c> {
-  constexpr double operator()(Array const &a) const {
+template <int I, typename T, std::size_t N, int c>
+struct inner_product_template_impl<I, T, N, c> {
+  constexpr T operator()(std::array<T, N> const &a) const {
     return (c) ? mul<c>(std::get<I>(a)) : 0.;
   }
 };
 
-template <typename T, std::size_t N, const std::array<int, N> &vec,
+template <typename T, std::size_t N,
+          const std::array<std::array<int, N>, N> &vec, std::size_t index,
           std::size_t... I>
-constexpr double inner_product_helper(const std::array<T, N> &n,
-                                      std::index_sequence<I...>) {
-  return inner_product_template_impl<0, std::get<I>(vec)...>{}(n);
+constexpr T inner_product_helper(const std::array<T, N> &n,
+                                 std::index_sequence<I...>) {
+  return inner_product_template_impl<0, T, N, vec[index][I]...>{}(n);
 }
 
-template <typename T, std::size_t N, const std::array<int, N> &vec,
+template <typename T, std::size_t N,
+          const std::array<std::array<int, N>, N> &vec, std::size_t index,
           typename Indices = std::make_index_sequence<N>>
-constexpr double inner_product_template(const std::array<T, N> &n) {
-  return inner_product_helper<T, N, vec>(n, Indices{});
+constexpr T inner_product_template(const std::array<T, N> &n) {
+  return inner_product_helper<T, N, vec, index>(n, Indices{});
 }
 
 } // namespace Utils
